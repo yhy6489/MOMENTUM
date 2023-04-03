7eeb64cb5cd228dc3f7ea1364a92b36a43e747ff
yhy6489 <earth3692@gmail.com>
2023-04-03 09:45:55 +0900

    4. LOGIN

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME ="hidden";
+const USER_KEY ="username";
+
+//login 버튼 클릭
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(event);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // greeting.innerText = "Hello "+ username;
+    localStorage.setItem(USER_KEY, username);
+    paintGreeting(username);
 }   
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+const savedUsername = localStorage.getItem(USER_KEY);
+
+if(savedUsername === null){
+    // form 보이기
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // hello
+    paintGreeting(savedUsername);
+}
