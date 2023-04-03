9acfd3661c7ad9049a78d9d8632ffc394b6f20c5
yhy6489 <earth3692@gmail.com>
2023-04-03 18:14:13 +0900

    시간 추가, 명언 진행중

---
+++ js/quotes.js
@@ -0,0 +1,27 @@
+const quotes = [
+    {
+        quote:"성공한 사람보다는 가치 있는 사람이 되라.",
+        author:"Albert Einstein",
+    },
+    {
+        quote:"웃음은 강장제이고, 안정제이며, 진통제이다.",
+        author:"Charlie Chaplin",
+    },
+    {
+        quote:"이 삶에서 우리가 자만심이 없다면 살아야 할 충분한 이유가 없다.",
+        author:"Lev Tolstoy",
+    },
+    {
+        quote:"미래는 현재 우리가 무엇을 하는가에 달려 있다.",
+        author:"Mahatma Gandhi",
+    },
+    {
+        quote:"실천이 말보다 낫다.",
+        author:"Benjamin Franklin",
+    }
+
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
